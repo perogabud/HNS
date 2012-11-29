a6d910677c68b3091feac19ab4ee151130b09726
pero <pero@debian.lan>
2012-11-29 17:20:56 +0100

    footer i home

--- app/view/elements/homeScripts.php
+++ app/view/elements/homeScripts.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="https://github.com/malsup/cycle/raw/master/jquery.cycle.lite.js"></script>
 <script type="text/javascript" src="/js/home.js"></script>
