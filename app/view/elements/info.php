fd9e48f5e7bb1eeae8c2f15007b7cf00d7b80acb
pero <pero@debian.lan>
2012-12-02 04:53:54 +0100

    peglanje detalja uglavnom homepage i subpages css

--- app/view/elements/info.php
+++ app/view/elements/info.php
@@ -4,7 +4,7 @@
   	<li><a href="/natjecanja/zupanijski-savezi"><img src="/img/zupanijski_savezi.png" alt="Županijski savezi" /></a></li>
   	<li><a href="http://www.prva-hnl.hr/"><img src="/img/1hnl.png" alt="1. HNL" /></a></li>
   	<li><a href="http://www.druga-hnl.com/"><img src="/img/2hnl.png" alt="2. HNL" /></a></li>
-  	<li><a href="http://www.hns-split.net/"><img src="/img/hns_sredista.png" alt="HNS središta" /></a></li>
+  	<li><a href="/hns/hns-sredista"><img src="/img/hns_sredista.png" alt="HNS središta" /></a></li>
   	<li><a href="http://www.hada.hr/"><img src="/img/hada.png" alt="Hrvatska agencija za borbu protiv dopinga u športu" /></a></li>
 		<li><a href="http://uvijekvjerni.hr" ><img src="/img/uvijek_vjerni.jpg" alt="UVIJEK VJERNI" /></a></li>
   </ul>
